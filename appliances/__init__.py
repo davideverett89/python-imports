585943f134784ce1f793833e64564cd81baaed97
David Everett <davideverett1989@gmail.com>
2020-07-16 19:56:04 -0500

    Fixed all errors in the intended way.

--- appliances/__init__.py
+++ appliances/__init__.py
@@ -1,1 +1,10 @@
 from .appliance import Appliance
+
+from .kitchen import CanOpener
+from .kitchen import CoffeeMaker
+from .kitchen import Stove
+from .kitchen import DishWasher
+from .kitchen import Refrigerator
+
+from .laundry import Washer
+from .laundry import Dryer
